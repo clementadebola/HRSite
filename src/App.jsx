dbce5331b693148a5b0d506455270b28e9687939
clement <clementadebola54@gmail.com>
2024-10-18 23:39:59 +0100

    new chnage

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import GlobalStyles from './styles/Globalstyles';
-import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
 
 const App = () => {
   return (
@@ -11,8 +9,6 @@ const App = () => {
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/services" element={<Services />} /> */}
       </Routes>
     </Router>
   );
