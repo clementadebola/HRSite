831c288636b04669b8c025c1e99bab3b70044370
clement <clementadebola54@gmail.com>
2024-10-18 23:34:10 +0100

    new

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import recuitmentImg from '../assets/recuitment.png'
-import trainingImg from '../assets/training.png'
-import employeeImg from "../assets/employee.png"
+import recuitmentImg from '../assets/recuitment.png';
+import trainingImg from '../assets/training.png';
+import employeeImg from "../assets/employee.png";
 
 const Services = () => {
   return (
@@ -10,27 +10,31 @@ const Services = () => {
       <h2>Our Services</h2>
       <ServiceGrid>
         <ServiceCard>
+          <CardContent>
             <div>
-            <h3>Recruitment</h3>
-            <p>Providing top-notch recruitment services to help you find the best talent.</p>
+              <h3>Recruitment</h3>
+              <p>Providing top-notch recruitment services to help you find the best talent.</p>
             </div>
-            <div>
-                <img src={recuitmentImg} alt="" />
-            </div>
+            <img src={recuitmentImg} alt="Recruitment Service" />
+          </CardContent>
         </ServiceCard>
         <ServiceCard>
-          <h3>Training & Development</h3>
-          <p>Enhancing employee skills through tailored training programs.</p>
-          <div>
-                <img src={trainingImg} alt="" />
+          <CardContent>
+            <div>
+              <h3>Training & Development</h3>
+              <p>Enhancing employee skills through tailored training programs.</p>
             </div>
+            <img src={trainingImg} alt="Training Service" />
+          </CardContent>
         </ServiceCard>
         <ServiceCard>
-          <h3>Employee Management</h3>
-          <p>Streamlining HR processes for better employee engagement and productivity.</p>
-          <div>
-                <img src={employeeImg} alt="" />
+          <CardContent>
+            <div>
+              <h3>Employee Management</h3>
+              <p>Streamlining HR processes for better employee engagement and productivity.</p>
             </div>
+            <img src={employeeImg} alt="Employee Management Service" />
+          </CardContent>
         </ServiceCard>
       </ServiceGrid>
     </ServicesSection>
@@ -91,5 +95,31 @@ const ServiceCard = styled.div`
     color: #333;
     font-size: 1rem;
     line-height: 1.6;
+  }
+`;
+
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 300px;
+  border-radius: 25px;
+
+  img {
+    max-width: 150px;
+    margin-top: 1.5rem;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    text-align: left;
+
+    img {
+      margin-left: 1.5rem;
+      margin-top: 0;
+    }
   }
 `;
